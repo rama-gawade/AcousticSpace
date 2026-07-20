c18a662e66c1e2f844151800dccfd225a77fd0b0
Rama Gawade <142524303+rama-gawade@users.noreply.github.com>
2026-07-20 22:27:32 +0530

    Create test_dataset.py

---
+++ backend/training/test_dataset.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+from app.database import get_connection
+
+conn = get_connection()
+
+cursor = conn.cursor()
+
+cursor.execute("""
+
+CREATE TABLE IF NOT EXISTS analysis_history(
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    filename TEXT,
+
+    prediction TEXT,
+
+    confidence REAL,
+
+    risk TEXT,
+
+    duration REAL,
+
+    sample_rate INTEGER,
+
+    analysis_time REAL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+)
+
+""")
+
+conn.commit()
+
+conn.close()
+
+print("Database Created Successfully!")
