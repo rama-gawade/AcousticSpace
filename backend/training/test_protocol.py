fab33ac6138fd4650fc45d454c87d829d0136e17
Rama Gawade <142524303+rama-gawade@users.noreply.github.com>
2026-07-21 22:34:24 +0530

    Weak 2 part

---
+++ backend/training/test_protocol.py
@@ -0,0 +1,15 @@
+
+from app.ai.dataset import load_train_protocol
+
+df = load_train_protocol()
+
+print(df.head())
+
+print("\nShape:")
+print(df.shape)
+
+print("\nColumns:")
+print(df.columns.tolist())
+
+print("\nLabel Counts:")
+print(df["label"].value_counts())
